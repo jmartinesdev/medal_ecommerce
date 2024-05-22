d2498992c676fc3542f8aaf1aed534109dcba960
Jakeline Martines <jakeline_13_martines@hotmail.com>
2024-05-22 02:20:58 +0100

    Added Categories and Product models

--- medalapp/models.py
+++ medalapp/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Categories(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    
+    def __str__(self):
+        return self.title
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    categories = models.ForeignKey(Categories, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="products")
+    price = models.PositiveBigIntegerField()
+    sell = models.PositiveBigIntegerField()
+    description = models.TextField()
+    views = models.PositiveBigIntegerField()
+    
